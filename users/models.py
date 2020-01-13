af52a34817e7b070a82c8315896d2f18aced119c
ElijahCoding <cuiyeqing960904@gmail.com>
2020-01-13 08:44:23 +0300

    user model built

--- users/models.py
+++ users/models.py
@@ -1,5 +1,66 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, _("Male")),
+        (GENDER_FEMALE, _("Female")),
+        (GENDER_OTHER, _("Other")),
+    )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_FRENCH = "fr"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, _("English")),
+        (LANGUAGE_FRENCH, _("French")),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_EUR = "euro"
+
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_EUR, "EUR"))
+
+    LOGIN_EMAIL = "email"
+    LOGIN_GITHUB = "github"
+    LOGING_KAKAO = "kakao"
+
+    LOGIN_CHOICES = (
+        (LOGIN_EMAIL, "Email"),
+        (LOGIN_GITHUB, "Github"),
+        (LOGING_KAKAO, "Kakao"),
+    )
+    
+    first_name = models.CharField(
+        _("first name"), max_length=30, blank=True, default="Unnamed User"
+    )
+    avatar = models.ImageField(upload_to="avatars", blank=True, default='')
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, null=True, blank=True
+    )
+    bio = models.TextField(
+        _('bio'), blank=True,
+    )
+    birthdate = models.DateField(blank=True, null=True)
+    language = models.CharField(
+        _('language'),
+        choices=LANGUAGE_CHOICES,
+        max_length=2,
+        blank=True,
+        default=LANGUAGE_ENGLISH
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES,
+        max_length=3,
+        blank=True,
+        default=CURRENCY_USD
+    )
+    superhost = models.BooleanField(default=False)
+    email_verified = models.BooleanField(default=False)
+    email_secret = models.CharField(max_length=20, default="", blank=True)
+    login_method = models.CharField(max_length=50, choices=LOGIN_CHOICES, default=LOGIN_EMAIL)
