56f564cf4a0b0c3dea91c8070f941febfa11ea9c
ElijahCoding <cuiyeqing960904@gmail.com>
2020-01-19 21:03:51 +0300

    check email

--- users/forms.py
+++ users/forms.py
@@ -4,3 +4,11 @@ from . import models
 class LoginForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        try:
+            models.User.objects.get(email=email)
+            return email
+        except models.User.DoesNotExist:
+            raise forms.ValidationError("User does not exist")
