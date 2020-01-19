82d5fe1651f6a9d69f24dc18ec6b9d99b0244069
ElijahCoding <cuiyeqing960904@gmail.com>
2020-01-19 17:37:56 +0300

    doing filtering

--- rooms/views.py
+++ rooms/views.py
@@ -30,16 +30,17 @@ class RoomDetail(DetailView):
 def search(request):
     city = request.GET.get("city", "Anywhere")
     city = str.capitalize(city)
-    country = request.GET.get("country", "US")
+    country = request.GET.get("country", "KR")
     room_type = int(request.GET.get("room_type", 0))
     price = int(request.GET.get("price", 0))
     guests = int(request.GET.get("guests", 0))
     bedrooms = int(request.GET.get("bedrooms", 0))
     beds = int(request.GET.get("beds", 0))
     baths = int(request.GET.get("baths", 0))
-    s_amenities = request.GET.get("amenities")
-    s_facilities = request.GET.get("facilities")
-    print(s_amenities, s_facilities)
+    instant = request.GET.get("instant", False)
+    super_host = request.GET.get("super_host", False)
+    s_amenities = request.GET.getlist("amenities")
+    s_facilities = request.GET.getlist("facilities")
 
     form = {
         "city": city,
@@ -50,6 +51,10 @@ def search(request):
         "bedrooms": bedrooms,
         "beds": beds,
         "baths": baths,
+        "s_amenities": s_amenities,
+        "s_facilities": s_facilities,
+        "instant": instant,
+        "super_host": super_host,
     }
 
     room_types = models.RoomType.objects.all()
